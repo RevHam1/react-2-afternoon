d15c9878e1345e969f3d072daff5f91ec0a0b8a4
RevHam1 <oldhamtyrone@yahoo.com>
2018-02-27 14:44:21 -0600

    stage 4 complete

--- src/Stage 4/models/Employee.js
+++ src/Stage 4/models/Employee.js
@@ -1,9 +1,28 @@
-export default class Employee {
   // constructor
-  
+  import React, { Component } from 'react';
+
+  class Employee extends Component {
+    constructor(id, name, phone, title) {
+      super()
+    this.id = id;
+    this.name = name;
+    this.phone = phone;
+    this.title = title;
+  }
+
   // updateName
+  updateName(name) {
+    this.name = name;
+  }
 
   // updatePhone
+  updatePhone(phone) {
+    this.phone = phone;
+  }
 
    // updateTitle
-}
+   updateTitle(title) {
+    this.title = title;
+  }
+  }
+  export default Employee 
