3504501c79b8fe7991536c50bd6ae85b7138269d
RevHam1 <oldhamtyrone@yahoo.com>
2018-02-27 21:04:35 -0600

    stage 7 complete

--- src/Stage 7/models/Employee.js
+++ src/Stage 7/models/Employee.js
@@ -1,9 +1,24 @@
-export default class Employee {
-  // constructor
-  
+class Employee{
+  constructor(id, name, phone, title) {
+    this.id = id;
+    this.name = name;
+    this.phone = phone;
+    this.title = title;
+  }
+
   // updateName
+  updateName(name) {
+    this.name = name;
+  }
 
   // updatePhone
+  updatePhone(phone) {
+    this.phone = phone;
+  }
 
-   // updateTitle
+  // updateTitle
+  updateTitle(title) {
+    this.title = title;
+  }
 }
+export default Employee; 
