87c60275bb7c9cd9d26cd42ee4cd61171dabeeeb
RevHam1 <oldhamtyrone@yahoo.com>
2018-03-06 01:38:38 -0600

    stage 9 complete

--- src/Stage 9/models/Employee.js
+++ src/Stage 9/models/Employee.js
@@ -1,9 +1,27 @@
-export default class Employee {
-  // constructor
-  
-  // updateName
+import React, { Component } from 'react';
 
-  // updatePhone
-
-   // updateTitle
+class Employee extends Component {
+  constructor(id, name, phone, title) {
+    super()
+  this.id = id;
+  this.name = name;
+  this.phone = phone;
+  this.title = title;
 }
+
+// updateName
+updateName(name) {
+  this.name = name;
+}
+
+// updatePhone
+updatePhone(phone) {
+  this.phone = phone;
+}
+
+ // updateTitle
+ updateTitle(title) {
+  this.title = title;
+}
+}
+export default Employee 
